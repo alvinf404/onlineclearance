d2baeb85daf7e7679cd118876954f6eec1e0dac1
alvinf404 <90584487+alvinf404@users.noreply.github.com>
2021-11-06 18:11:12 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include('connect.php')
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<link rel="stylesheet" href="production/stylesheet.css">
 	<!-- icon -->
 	<link rel="icon" href="images/green-chmsc-official-logo.png" >
@@ -173,8 +173,8 @@ include('connect.php')
 				</div>
 			</div>
 		</div>
-		<script src="js/jquery-1.12.1.min.js" type="text/javascript"></script>
-		<script src="js/bootstrap.min.js" type="text/javascript"></script>
+		<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 		<script>
 			$('.carousel').carousel({
         interval: 3000 //changes the speed
